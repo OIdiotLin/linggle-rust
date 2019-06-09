574a5e088634caee80b338f1bc0285e768290462
Huidong Lin <oidiotlin@gmail.com>
2019-06-09 23:27:01 +0800

    api finished

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #[macro_use]
 extern crate lazy_static;
 extern crate reqwest;
+extern crate dirs;
+extern crate chrono;
 
 mod linggle;
+mod storage;
 
 fn main() {
     let res = linggle::query("what _ day").unwrap();
